c22caef116a57baa92320dc68c89b6d4aad4e7aa
Amal Krishna R <amalrkrishna94@gmail.com>
2016-07-06 17:41:48 +0530

    Update run.py

--- P3/run.py
+++ P3/run.py
@@ -12,6 +12,9 @@ Write a python program to cater basic web container needs. Your program should b
 3) http request handling
 
 Scenario: Suppose we have three python scripts say urls.py, controller.py, run.py and one html index.html where urls.py shall contain urls to controller and template mapping, controller.py should return template context and run.py to handle server functions. So, this python script should be able to render this html with its context on browser and hence should contain a simple http server inbuilt. Kindly keep this implementation limited to simple and basic web page rendering.
+
+Note: The Project has used default deployment files from Django project. I needed to work mainly only with the templates and run.py, controller.py and urls.py.  
+
 """
 
 __author__		= "Amal Krishna R"
