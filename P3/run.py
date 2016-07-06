895b6b6816ea5025a25622843df985fc724317db
amalrkrishna <amalrkrishna94@gmail.com>
2016-07-06 12:16:14 +0530

    P3 added

--- P3/run.py
+++ P3/run.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+
+"""  $ python run.py runserver 0:8080  """
+
+"""
+Programming test: Basic Web Container
+
+Write a python program to cater basic web container needs. Your program should be a mini web framework which should be able for:
+
+1) URL mapping to controller
+2) template rendering
+3) http request handling
+
+Scenario: Suppose we have three python scripts say urls.py, controller.py, run.py and one html index.html where urls.py shall contain urls to controller and template mapping, controller.py should return template context and run.py to handle server functions. So, this python script should be able to render this html with its context on browser and hence should contain a simple http server inbuilt. Kindly keep this implementation limited to simple and basic web page rendering.
+"""
+
+__author__		= "Amal Krishna R"
+__date__		= "06/07/2016"
+
 import os
 import sys
 
